a8e7ac951415bc14ee1dafe3f4155dda37dcc8d5
Abari Francis <34786531+fopioabari@users.noreply.github.com>
2018-03-26 18:56:35 -0500

    Update Convert_celsius_to_Fahrenheit_hw_2_ex_2.1.py

--- Assignment_2/Convert_celsius_to_Fahrenheit_hw_2_ex_2.1.py
+++ Assignment_2/Convert_celsius_to_Fahrenheit_hw_2_ex_2.1.py
@@ -6,17 +6,9 @@ Created on Tue Jan 30 05:11:57 2018
 @author: francisabari
 """
 
-#Home Work_2_Ex_2.1 (convert Celsius to Fahrenheit)
-
-#First prompt the user for celsius value
-
 celsius=eval(input("Enter a degree in Celsius:",))
 
-#Then convert Celsius to Fahrenheit using formulae  
-
 fahrenheit = (9 / 5) * celsius + 32
-
-#Lastly print the two values
 
 print(celsius, " celsius is ",fahrenheit, " fahrenheit.")
 
