4650c88ab8c2430a86e8709840e87037d6438053
Abari Francis <34786531+fopioabari@users.noreply.github.com>
2018-03-26 18:55:43 -0500

    Update Area_of_regualr_polygon_hw_2_ex_3.5.py

--- Assignment_2/Area_of_regualr_polygon_hw_2_ex_3.5.py
+++ Assignment_2/Area_of_regualr_polygon_hw_2_ex_3.5.py
@@ -5,21 +5,10 @@ Created on Tue Jan 30 05:31:58 2018
 
 @author: francisabari
 """
-#Home Work_2_Ex_3.5 (Geometry:area of a regular polygon)
-
-#How to calculate the area of pentagon using side
 
 import math
 
-#Get number of sides and length of side from user
-
 number = eval(input("Enter the number of sides:"))
 side = eval(input("Enter the side:"))
-
-#Compute the area using length of side
-
 area = number *side *side/ math.tan(math.pi /number) /4
-
-#Display the area of polygon 
-
 print("The area of the polygon is " + format(area, "<10.2f"))
